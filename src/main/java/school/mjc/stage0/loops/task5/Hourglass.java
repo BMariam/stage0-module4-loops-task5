31c68973266cb0fbb9357f393132f7a0cf5b5101
BMariam <mariambaghdasaryan77@gmail.com>
2023-08-15 01:52:48 +0400

    Updated files

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,23 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+		boolean heightIsEven = (0 == height % 2) ? true : false;
+		int center = heightIsEven ? (height / 2 - 1) : (height / 2);
+		for (int i = 0; i < height; ++i) {
+			int distanceFromEdge = center - Math.abs(i - center);
+			if (heightIsEven && i > center) {
+				distanceFromEdge = center - Math.abs(i - center) + 1;
+			}
+			for (int j = 0; j < distanceFromEdge; ++j) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < height - 2 * (distanceFromEdge); ++j) {
+				System.out.print("8");
+			}
+			for (int j = 0; j < distanceFromEdge; ++j) {
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
     public void printSquareFrom8s(int sideLength){
-
+		for (int i = 1; i <= sideLength; ++i) {
+			if (1 == i || sideLength == i) {
+				for (int j = 1; j <= sideLength; ++j) {
+					System.out.print("8");
+				}
+			} else {
+				System.out.print("8");
+				for (int j = 2; j < sideLength; ++j) {
+					System.out.print(" ");
+				}
+				System.out.print("8");
+			}
+			System.out.println();
+		}
     }
 }
